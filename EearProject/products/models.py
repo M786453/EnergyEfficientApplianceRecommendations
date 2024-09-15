d94d644fb6f2ccd1245f758b3761299563ea5b91
M786453 <ahteshamsarwar333@gmail.com>
2024-09-15 14:08:52 +0500

    Appliance model added.

--- EearProject/products/models.py
+++ EearProject/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Appliance(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    url = models.URLField()
+
+    image = models.URLField()
+
+    price = models.FloatField()
+
+    availability = models.CharField(max_length=20)
+
+    power = models.CharField(max_length=20)
+
+    voltage = models.CharField(max_length=20)
